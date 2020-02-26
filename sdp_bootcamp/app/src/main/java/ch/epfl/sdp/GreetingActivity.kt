25f078ad6a8d6d7a41700fa4428bfa28871a4a54
Nicolas Bähler <nicolas.bahler@epfl.ch>
2020-02-26 07:54:41 +0100

    Apps works, but last test fails

---
+++ sdp_bootcamp/app/src/main/java/ch/epfl/sdp/GreetingActivity.kt
@@ -0,0 +1,21 @@
+package ch.epfl.sdp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class GreetingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_greeting)
+
+        // Get the Intent that started this activity and extract the string
+        val message = intent.getStringExtra(EXTRA_MESSAGE)
+
+        // Capture the layout's TextView and set the string as its text
+        val textView = findViewById<TextView>(R.id.greetingMessage).apply {
+            text = "Hello $message"
+        }
+    }
+}
